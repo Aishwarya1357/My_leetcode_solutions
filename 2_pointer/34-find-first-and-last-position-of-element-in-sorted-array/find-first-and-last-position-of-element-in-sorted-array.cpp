16e38f37e461b747968ec1be824838daac6d2570
Aishwarya1357 <137132207+Aishwarya1357@users.noreply.github.com>
2024-09-25 23:44:17 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (42.33%) - LeetSync

--- 2_pointer/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 2_pointer/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,56 @@
 class Solution {
 public:
+
+    int firstoccurence(vector<int>& nums, int target) {
+        int fo = -1;
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;  // Correct calculation of mid
+
+            if (nums[mid] > target) {
+                r = mid - 1;
+            } 
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            } 
+            else {
+                fo = mid;  // Update first occurrence
+                r = mid - 1;  // Continue searching in the left half
+            }
+        }
+        return fo;
+    }
+
+    int lastoccurence(vector<int>& nums, int target) {
+        int fl = -1;
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;  // Correct calculation of mid
+
+            if (nums[mid] > target) {
+                r = mid - 1;
+            } 
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            } 
+            else {
+                fl = mid;  // Update last occurrence
+                l = mid + 1;  // Continue searching in the right half
+            }
+        }
+        return fl;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
+        int fo = firstoccurence(nums, target);  // Correct function call
+        int fl = lastoccurence(nums, target);   // Correct function call
 
-
-        vector<int>result(2,-1);
-
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                result[0]=i;
-                break;
-            }
-        }
-
-        for(int j=nums.size()-1;j>=0;j--){
-            if(nums[j]==target){
-                result[1]=j;
-                break;
-            }
-        }
-
-        return result;}
-
-
-
+        vector<int> result = {fo, fl};  // Correct initialization of result vector
+        return result;
+    }
 };
+
