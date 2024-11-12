2feb52b62cdc0e01fca054f4596b5ba031a7d008
Aishwarya1357 <137132207+Aishwarya1357@users.noreply.github.com>
2024-11-12 15:42:59 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (90.71%) - LeetSync

--- GREEDY/268-missing-number/missing-number.cpp
+++ GREEDY/268-missing-number/missing-number.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
 
-int N=nums.size();
-int sum=(N*(N+1))/2;
-int arsum=0;
-for(int i=0;i<N;i++){
-    arsum=arsum+nums[i];
+        int XOR1=0;
+        int XOR2=0;
+        int N=nums.size();
+        for(int i=0;i<N;i++){
+            XOR1=XOR1^nums[i];
+            XOR2=XOR2^(i+1);
 
-}
+        }
+        return XOR1^XOR2;
 
-return (sum-arsum);
+
 
 
         
