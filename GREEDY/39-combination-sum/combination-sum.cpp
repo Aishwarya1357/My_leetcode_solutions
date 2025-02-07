e8871d6d658f1afbe303507e48c2391f69fed830
Aishwarya1357 <137132207+Aishwarya1357@users.noreply.github.com>
2025-02-08 02:25:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (74.62%) - LeetSync

--- GREEDY/39-combination-sum/combination-sum.cpp
+++ GREEDY/39-combination-sum/combination-sum.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
 
-void combinationsum(int ind, int target, vector<int>&arr, vector<vector<int>>&ans, vector<int>&ds){
+void comb(int ind, int target, vector<int>&arr, vector<int>&ds, vector<vector<int>>&ans){
     if(ind==arr.size()){
         if(target==0){
-            ans.push_back(ds);
-        }
-
-            return;
+            ans.push_back(ds);}
         
+        return;
+
     }
-
-   
-
 
     if(arr[ind]<=target){
         ds.push_back(arr[ind]);
-        combinationsum(ind, target-arr[ind], arr, ans, ds);
-        ds.pop_back();
-
+    comb(ind, target-arr[ind], arr, ds, ans);
+  
+    ds.pop_back();
     }
+    
 
-    combinationsum(ind+1, target, arr,ans,ds);
+    comb(ind+1, target, arr,ds,ans);
+
+
 
 }
 
 
 
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<int>ds;
+    vector<vector<int>>ans;
+
+    comb(0, target, candidates, ds, ans);
+    return ans;
 
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-
-        vector<vector<int>>ans;
-        vector<int>ds;
-        combinationsum(0, target, candidates,ans,ds);
-        return ans;
+      
 
 
         
