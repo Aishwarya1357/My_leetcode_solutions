4d17b2d0587359e3f37e80a6597a5afca0e2fb1a
Aishwarya1357 <137132207+Aishwarya1357@users.noreply.github.com>
2025-02-20 13:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (88.59%) - LeetSync

--- GREEDY/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ GREEDY/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen=0;
-        int len;
+        int n = s.size();
+        int maxlen = 0;
+        int l = 0, r = 0;
+        int hash[256]; 
+        
+        memset(hash, -1, sizeof(hash)); // Initialize the entire array to -1
 
-        for(int i=0;i<s.size();i++){
-         int   hash[256]={0};
-         for(int j=i;j<s.size();j++){
-            if(hash[s[j]]==1)
-            break;
-            len=j-i+1;
-            maxlen= max(maxlen, len);
-            hash[s[j]]=1;
-         }
-        
-        
+        while (r < n) {
+            if (hash[s[r]] >= l) {  
+                l = hash[s[r]] + 1;
+            }
+
+            maxlen = max(maxlen, r - l + 1);
+            hash[s[r]] = r; // Update last occurrence index
+            r++;
         }
-
         return maxlen;
-        
     }
 };
